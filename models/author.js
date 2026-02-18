4d562c7eeece7590457a8c9f749215f05c100d7b
Caroline Knight Henderson <chend227@pratt.edu>
2026-02-17 20:09:22 -0500

    created author model, previously unstaged changes

---
+++ models/author.js
@@ -0,0 +1,7 @@
+const authors = [
+    {firstName: "James S.A.", lastName: "Corey"}, 
+    {firstName: "Craig", lastName: "Alanson"}, 
+    {firstName: "Cixin", lastName: "Liu"}
+]
+
+exports.all = authors;
--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const Author = require('../models/author');
+
 router.get('/', function(_req, res, _next) {
-  const authors = [
-    {firstName: "James S.A.", lastName: "Corey"}, 
-    {firstName: "Craig", lastName: "Alanson"}, 
-    {firstName: "Cixin", lastName: "Liu"}
-  ]
+  const authors = Author.all
+  // const authors = [
+  //   {firstName: "James S.A.", lastName: "Corey"}, 
+  //   {firstName: "Craig", lastName: "Alanson"}, 
+  //   {firstName: "Cixin", lastName: "Liu"}
+  // ]
   res.render('authors/index', {title: 'bookedin || authors', authors: authors});
 });
 
