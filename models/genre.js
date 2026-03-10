1731c7be185720bbd35dba83a8d9d9c2a6b7fddb
caroline-knight <carolineknighth@gmail.com>
2026-03-10 16:53:45 -0400

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -13,17 +13,17 @@ exports.upsert = (genre) => {
   else {
     exports.add(genre);
   }
-}
+};
 
 exports.add = (genre) => {
   genres.push(genre);
-}
+};
 
 exports.update = (genre) => {
   genre.id = parseInt(genre.id);
   genres[genre.id] = genre;
-}
+};
 
 exports.get = (idx) => {
   return genres[idx];
-}
+};
