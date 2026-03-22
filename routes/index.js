5485613d48396cbaa018c6aabb51362891a56fac
Rik Vanmechelen <rik_vanmechelen@gmail.com>
2026-03-22 09:27:55 -0400

    some changes to make your code run

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router(); // define the router
 
 router.get('/', function(req, res, next) {
     res.render('index', {title: 'bookedin'}); // rendering an object (title)
-}); 
+});
 
 router.get('/about', function(req, res, next) {
-    res.render('about', {title: 'bookedin');
+    res.render('about', {title: 'bookedin'});
 });
 
 module.exports = router; // JS requires exporting what is used in the other files.
