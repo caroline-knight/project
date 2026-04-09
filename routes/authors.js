71ae2dfcda85ee28c85f428bf5e358b3d0c776df
caroline-knight <carolineknighth@gmail.com>
2026-04-08 23:20:45 -0400

    Update authors.js

--- routes/authors.js
+++ routes/authors.js
@@ -1,38 +1,38 @@
 const express = require('express');
-const router = express.Router();
 
 // '..' is used because you have to go up a directory. in contrast, '.' references your current directory.
 const Author = require('../models/author'); 
 
-// connect to author model
+const router = express.Router();
+
+// connect to author model - call function from author model to get all data and wait for it to complete
 router.get('/', function(req, res, next) {
-  const authors = Author.all;
-  res.render('authors/index', {title: 'bookedin || authors', authors: authors});
+  res.render('authors/index', { title: 'bookedin || authors', authors: Author.all });
 });
 
 // connect to author input form
-router.get('/form', async (req, res, next) => {
-  res.render('authors/form', {title: 'bookedin || authors'});
+router.get('/form', function(req, res, next) {
+  res.render('authors/form', { title: 'bookedin || authors' });
 });
 
 // makes req.body into string because it is an object and needs to be printable. calls the model's 'add' method. redirects back to authors index page. replaced create with upsert, which stands for UPdate or inSERT, since we use this to both update and create.
-router.post('/upsert', async (req, res, next) => {
-  console.log('body: ' + JSON.stringify(req.body));
+router.post('/upsert', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
   Author.upsert(req.body);
-  let createdOrupdated = "id" in req.body ? 'updated' : 'created';
+  let createdOrupdated = req.body.id ? 'updated' : 'created';
   req.session.flash = {
     type: 'info',
     intro: 'success!',
     message: `this author has been ${createdOrupdated}!`,
   };
-  res.redirect(303, '/authors');
+  res.redirect(303, "/authors");
 });
 
 // to handle the edit route
-router.get('/edit', async (req, res, next) => {
-  let authorIndex = req.query.id;
-  let author = Author.get(authorIndex);
-  res.render('authors/form', {title: 'bookedin || authors', author: author, authorIndex: authorIndex });
+router.get('/edit', function(req, res, next) {
+  let authorIdx = req.query.id
+  let author = Author.get(authorIdx);
+  res.render('authors/form', { title: 'bookedin || authors', author: author, authorIdx: authorIdx });
 });
 
 module.exports = router;
